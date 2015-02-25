c47963bd9560565db6be0a4d37404813b636b611
Athul Acharya <aacharya@gmail.com>
2015-02-24 17:00:02 -0800

    Got rid of appcompat.

--- app/src/main/java/com/athulacharya/trackn/TrackApp.java
+++ app/src/main/java/com/athulacharya/trackn/TrackApp.java
@@ -1,6 +1,6 @@
 package com.athulacharya.trackn;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 
-public class TrackApp extends ActionBarActivity {
+public class TrackApp extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/athulacharya/trackn/TrackWidgetProvider.java
+++ app/src/main/java/com/athulacharya/trackn/TrackWidgetProvider.java
@@ -5,9 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.RemoteViews;
 
 public class TrackWidgetProvider extends AppWidgetProvider {
