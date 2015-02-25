9d06069fa182fedcd747f14996b7dade582948ec
Athul Acharya <aacharya@gmail.com>
2015-02-25 07:25:15 -0800

    Rudimentary handlers for the dialog buttons.

--- app/src/main/java/com/athulacharya/trackn/AddNDialogActivity.java
+++ app/src/main/java/com/athulacharya/trackn/AddNDialogActivity.java
@@ -2,6 +2,7 @@ package com.athulacharya.trackn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class AddNDialogActivity extends Activity {
@@ -10,5 +11,14 @@ public class AddNDialogActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_addn);
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void add(View view) {
+        System.out.println("ok!");
+        finish();
     }
 }
